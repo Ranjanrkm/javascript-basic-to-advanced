a86271ec830419d897650adb8b5c1ace3cb0f06a
Ranjanrkm <ranjan2003krmahato12@gmail.com>
2026-04-15 17:50:34 +0530

    Start JS course - Module 1 added

---
+++ Module 1 - Basics of javascript/1.hello geeks.js
@@ -0,0 +1,9 @@
+/**
+ * First Program - Show Hello Geeks on Console
+ * Understand what is console.
+*/
+
+console.log('Hello Geeks!!');
+console.log("My name is Ranjan Kumar Mahato!")
+
+console.log(17 + 13);
