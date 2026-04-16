3fe4d68115230bce21b263702659fc35f61cf666
Ranjanrkm <ranjan2003krmahato12@gmail.com>
2026-04-16 19:58:56 +0530

    arithmatic covered

---
+++ Module 1 - Basics of javascript/07. arithmetic.js
@@ -0,0 +1,59 @@
+
+const x = 12;
+const y = 3;
+
+console.log(x + y);
+
+console.log(x - y);
+
+console.log(x * y);
+
+console.log(x / y);
+
+console.log(x % y);
+
+console.log(x ** 2);
+
+console.log(y ** 3);
+
+console.log("with double quote");
+
+const a = "12";
+const b = "3";
+
+console.log(a + b);
+
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a ** 2);
+
+console.log(b ** 3);
+
+console.log("with number and string");
+const m = 12;
+const n = "3";
+
+console.log(m + n);
+console.log(m - n);
+console.log(m * n);
+console.log(m / n);
+console.log(m % n);
+console.log(m ** 2);
+
+console.log(n ** 3);
+
+console.log("with string")
+
+const p = "Mango";
+const q = "Apple";
+
+console.log(p + q);
+console.log(p - q);
+console.log(p * q);
+console.log(p / q);
+console.log(p % q);
+console.log(p ** 2);
+
+console.log(q ** 3);
