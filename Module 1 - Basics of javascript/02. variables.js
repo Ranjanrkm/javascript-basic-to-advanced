aba54d0a352c00ed377b7ebc8279f158d18642cc
Ranjanrkm <ranjan2003krmahato12@gmail.com>
2026-04-16 18:29:06 +0530

    variables completed

---
+++ Module 1 - Basics of javascript/02. variables.js
@@ -0,0 +1,28 @@
+/**
+ * Lesson - 2
+ * Why Variables 🚀
+ * What is a Variable 🔥
+ * Different ways to create variables ⭐
+ */
+
+// var message;
+
+// message = "Hello GeeksforGeeks!";
+
+// message = "My name is Ranjan Kumar Mahato!";
+
+// console.log(message);
+
+let text; //Creating Variable or Declaring a variable
+
+text = "JavaScript is the best!!!!"; // Assigning a value to a variable
+
+// console.log(text);
+
+// let myname = "Ranjan Kumar Mahato!"
+
+// console.log(myname);
+
+const number = 11;
+
+console.log(number);
