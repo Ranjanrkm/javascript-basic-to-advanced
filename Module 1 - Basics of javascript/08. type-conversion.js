1689763e7e99397cb1f7070ff277853ea07d65b6
Ranjanrkm <ranjan2003krmahato12@gmail.com>
2026-04-17 15:54:32 +0530

    types conversion completed

---
+++ Module 1 - Basics of javascript/08. type-conversion.js
@@ -0,0 +1,28 @@
+/** 
+ * Type Conversion 🌟🌟
+ * to String..
+ * to Number..
+ * to Boolean..
+*/
+
+
+const a = "Ranjan";
+const b = 0;
+
+
+console.log(Boolean(a));
+console.log(Boolean(b));
+
+const x = "3";
+const y = "10";
+console.log(x + y);
+console.log(Number(x) + Number(y));
+
+
+// const c = String(a);
+// const d = String(b);
+
+console.log(typeof a);
+console.log(typeof b);
+// console.log(typeof c);
+// console.log(typeof d);
