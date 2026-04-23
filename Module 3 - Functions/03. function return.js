5b87ce6a2497654e979833a1ea449d8c9af80bee
Ranjanrkm <ranjan2003krmahato12@gmail.com>
2026-04-23 11:53:33 +0530

    completed return function

--- Module 3 - Functions/03. function return.js
+++ Module 3 - Functions/03. function return.js
@@ -7,10 +7,10 @@ function calculateSum(x, y){
     return x + y;
 }
 
-// console.log(calculateSum(12, 15));
+console.log(calculateSum(12, 15));
 
 const result = calculateSum(13, 21);
 console.log(result);
 
-// const output = calculateSum(99, 101);
-// console.log(output);
+const output = calculateSum(99, 101);
+console.log(output);
